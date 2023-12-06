f242c4e500cc61be9ffdd5aac687e4de7dee7801
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2023-12-05 19:26:38 -0500

    Feat: Add model for createSesskey response

---
+++ app/src/main/java/com/juandiegogarcia/loginusertimetonic/SessionModelResponse.kt
@@ -0,0 +1,21 @@
+package com.juandiegogarcia.loginusertimetonic
+
+data class SessionModelResponse(
+    val status: String,
+    val sesskey: String?,
+    val id: String?,
+    val restrictions: Restrictions?,
+    val appName: String?,
+    val createdVNB: String?,
+    val req: String?
+)
+
+data class Restrictions(
+    val carnet_code: String?,
+    val carnet_owner: String?,
+    val readonly: Boolean,
+    val hideTables: Boolean,
+    val hideMessages: Boolean,
+    val hideEvents: Boolean,
+    val internal: Boolean
+)
