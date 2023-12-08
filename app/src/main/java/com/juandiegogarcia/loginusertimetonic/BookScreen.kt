571ef5448518d6994506a442a58acdc1fbe4ad85
juandiego11116@gmail.com <71513912+juandiego11116@users.noreply.github.com>
2023-12-08 12:55:28 -0500

    Feat: create temporality screen for show description

--- app/src/main/java/com/juandiegogarcia/loginusertimetonic/BookScreen.kt
+++ app/src/main/java/com/juandiegogarcia/loginusertimetonic/BookScreen.kt
@@ -3,8 +3,23 @@ package com.juandiegogarcia.loginusertimetonic
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.runBlocking
 
 @Composable
-fun BookScreen(sessionKey:String){
+fun BookScreen(o_u:String, sessionKey:String){
+    val context = LocalContext.current
+    Column {
 
+        val books = runBlocking {
+            val booksResponse = BooksFetch(o_u = o_u, sessionKey)
+            return@runBlocking booksResponse
+        }
+
+        if (books == null){
+            showMessage(context, message = "Your user or password is wrong")
+        }else{
+            books.allBooks.books[0].description?.let { Text(text = it) }
+        }
+    }
 }
